26c8e58247e03d99ecce691ed016bafbaacbb328
owlisnotacat1 <118146757+owlisnotacat1@users.noreply.github.com>
2024-12-19 00:25:40 -0600

    fix branch error that would crash wii

--- ASM/src/hacks/z_kaleido_scope.asm
+++ ASM/src/hacks/z_kaleido_scope.asm
@@ -2,45 +2,44 @@
 
 .headersize(0x808137C0 - 0x00BB11E0)
 
+; modified relocation entries
+.org 0x8082FBFC
+    .word 0x45012D1C ; should relocate hi(0x8082A388)
+    .word 0x46012D24 ; should relocate lo(0x8082A388)
+.org 0x8082F780
+    .word 0x4500DB7C ; should relocate hi(0x80829E40)
+    .word 0x4600DB84 ; should relocate lo(0x80829E40)
+
 .org 0x8082389C
-    slti    at, v0, 20
-    beqz    at, 0x808238B0
     nop
-    b       kaleidoScope_Set_yOffset
+    jal       kaleidoScope_Set_yOffset
     nop
-kaleidoScope_Set_yOffset_return:
+    bne       at, zero, 0x808238B0
+    nop
 
 .org 0x80822538
-    b       kaleidoScope_draw_select_hack
+    jal     kaleidoScope_draw_select_hack
     nop
-kaleidoScope_draw_select_hack_return:
+    bne     at, zero, 0x808226D4
+    nop
 
 .org 0x808264dc
-    b       kaleidoScope_SwitchCaseHack ; this branch is to a modified version of the pauseCtx->state switch case
-    ori     at, at, 0x760           ; replaced code
+    lui     a1, 0x8082
+    jal     kaleidoScope_SwitchCaseHack ; this branch is to a modified version of the pauseCtx->state switch case
+    addiu   a1, 0xA388
+    beq     at, v0, 0x808287D8 ; jump to case 17 if v0 is 2
     nop
-    nop
-    nop
-    nop
-@@return:
+    beqz    v0, 0x80828BE4 ; jump to return if v0 is 0 else continue flow
     nop
 
 .org 0x80820d94
-    b       KaleidoScope_DrawPages_ConditionHack
+    jal       KaleidoScope_DrawPages_ConditionHack
+    nop
+    bne       at, zero, 0x80821580
     nop
     nop
     nop
     nop
-    nop
-    nop
-KaleidoScope_DrawPages_ConditionHack_return:
-
-.org 0x8082FBFC
-    .word 0x45013ED4 ; should relocate hi(0x8082A388)
-    .word 0x46013ED8 ; should relocate lo(0x8082A388)
-.org 0x8082F780
-    .word 0x45013FA8 ; should relocate hi(0x80829E40)
-    .word 0x46013FB8 ; should relocate lo(0x80829E40)
 
 .org 0x8081E978
     jal     KaleidoScope_UpdatePrompt
@@ -57,7 +56,10 @@ KaleidoScope_DrawPages_ConditionHack_return:
     nop
 
 .org 0x8082133C
-    b       KaleidoScope_Draw_ContinuePromtHack
+    lui     a1, 0x8083
+    jal     KaleidoScope_Draw_ContinuePromtHack
+    lw      a1, -0x61C0(a1)
+    bne     at, zero, 0x808214F0
     nop
     nop
     nop
@@ -67,111 +69,4 @@ KaleidoScope_DrawPages_ConditionHack_return:
     nop
     nop
     nop
-    nop
-    nop
-    nop
-    nop
-KaleidoScope_Draw_ContinuePromtHack_return:
-
-.org 0x80827690
-kaleidoScope_SwitchCaseHack:
-    addu    s0, s1, at
-    lui     at, 0x8082
-    addiu   at, 0xA388
-    lui     t8, hi(D_8082B25C_RELOCATED)
-    addiu   t8, t8, lo(D_8082B25C_RELOCATED)
-    sw      at, 0(t8)
-    lhu     t8, 0x01D4(s0)          ; loads pauseCtx->state into t8
-    ori     at, zero, 20
-    beq     t8, at, @@case20        ; jumps to case 20 function (not working yet but thats not the issue)
-    addiu   at, at, 1               ; increment at to 21
-    beq     t8, at, @@case21        ; jumps to case 21 function (not working yet but thats not the issue)
-    addiu   at, at, 1               ; increment at to 22
-    beq     t8, at, @@case22        ; jumps to case 22 (identical to case 17 but pauseCtx->state is referenced by other code and it being 17 causes the game over text to be drawn)
-    addiu   t9, t8, 0xFFFD          ; replaced code from above
-    sltiu   at, t9, 0x0011          ; replaced code from above
-    beq     at, zero, 0x80828BE4    ; replaced code from above
-    sll     t9, t9, 0x0002          ; replaced code from above
-    b       0x808264f4              ; returns back to @@return to continue if none of the new or modified cases are being called and returns to the original code
-    nop
-@@case20:
-    jal     kaleidoScope_Case20
-    or      a0, s1, zero
-    b       0x80828BE4              ; jumps to return section of kaleidoScope_Update
-    nop
-@@case21:
-    jal     kaleidoScope_Case21
-    or      a0, s1, zero
-    b       0x80828BE4              ; jumps to return section of kaleidoScope_Update
-    nop
-@@case22:
-    b       0x808287D8              ; jumps to case 17
-    nop
-
-;0x8039CAA4
-KaleidoScope_DrawPages_ConditionHack:
-    addiu   at, zero, 7
-    beq     a0, at, 0x80820db0
-    addiu   at, zero, 21
-    beq     a0, at, 0x80820db0
-    addiu   at, zero, 20
-    beq     a0, at, 0x80820db0
-    slti    at, a0, 0x0008
-    bne     at, zero, 0x80821580
-    slti    at, a0, 0x0012
-    beql    at, zero, 0x80821584
-    lw      ra, 0x0024(sp)
-    b       KaleidoScope_DrawPages_ConditionHack_return
-    nop
-KaleidoScope_Draw_ContinuePromtHack:
-    addiu   at, zero, 0x10
-    or      s6, zero, a0
-    beq     a0, at, KaleidoScope_Draw_Texture_Logic
-    li      at, 0x14
-    beq     a0, at, KaleidoScope_Draw_Texture_Logic
-    li      at, 0x15
-    beq     a0, at, KaleidoScope_Draw_Texture_Logic
-    li      at, 0x11
-    bnel    a0, at, 0x808214F0
-    lw      v1, 0x2C0(s0)
-KaleidoScope_Draw_Texture_Logic:
-    lui     t6, 0x8012
-    lbu     t6, -0x4627(t6)
-    lui     a1, 0x8083  ; relocate
-    lw      a0, 0x02C0(s0)
-    sll     t8, t6, 2
-    addu    a1, a1, t8
-    lw      a1, -0x61C0(a1) ; relocate
-    ;logic for loading a1 if needed
-    ori     at, zero, 0x13
-    slt     at, at, s6
-    ori     s6, zero, 0
-    beqz    at, KaleidoScope_TexUnchanged
-    nop
-    lui     a1, hi(ICON_STATIC_GAME_OVER)
-    addiu   a1, a1, lo(ICON_STATIC_GAME_OVER)
-    lw      a1, 0(a1)
-    addiu   a1, a1, 0x2980
-KaleidoScope_TexUnchanged:
-    sw      zero, 0x10(sp)
-    addiu   a2, zero, 0x0098
-    b       KaleidoScope_Draw_ContinuePromtHack_return
-    nop
-kaleidoScope_Set_yOffset:
-    lui     at, 0x0001
-    ori     at, at, 0x0760
-    addu    t0, a0, at
-    addiu   t7, zero, 0x0050
-    sh      t7, 0x020A(t0)
-    b       kaleidoScope_Set_yOffset_return
-    nop
-kaleidoScope_draw_select_hack:
-    ori     at, zero, 0x0007
-    beq     a1, at, kaleidoScope_draw_select_hack_return
-    ori     at, zero, 0x0014
-    beq     a1, at, kaleidoScope_draw_select_hack_return
-    addiu   at, at, 0x0001
-    beq     a1, at, kaleidoScope_draw_select_hack_return
-    nop
-    b       0x808226d4
     nop
--- ASM/src/pause.asm
+++ ASM/src/pause.asm
@@ -46,13 +46,158 @@ lbl_8009A15C:
     nop
     nop
 
+kaleidoScope_SwitchCaseHack:
+    addiu   sp, sp, -0x0014
+    sw      ra, 0x0010(sp)
+    ori     at, at, 0x760
+    addu    s0, s1, at
+    lui     t8, hi(D_8082B25C_RELOCATED)
+    addiu   t8, t8, lo(D_8082B25C_RELOCATED)
+    sw      a1, 0(t8)
+    lhu     t8, 0x01D4(s0)          ; loads pauseCtx->state into t8
+    ori     at, zero, 20
+    beq     t8, at, case20        ; jumps to case 20 function (not working yet but thats not the issue)
+    addiu   at, at, 1               ; increment at to 21
+    beq     t8, at, case21        ; jumps to case 21 function (not working yet but thats not the issue)
+    addiu   at, at, 1               ; increment at to 22
+    beq     t8, at, case22        ; jumps to case 22 (identical to case 17 but pauseCtx->state is referenced by other code and it being 17 causes the game over text to be drawn)
+    addiu   t9, t8, 0xFFFD          ; replaced code from above
+    sltiu   at, t9, 0x0011          ; replaced code from above
+    beq     at, zero, returnFromSwitchHack    ; replaced code from above ; return section
+    ori     v0, zero, 0
+    sll     t9, t9, 0x0002          ; replaced code from above
+    b       return_no_new_cases              ; returns back to @@return to continue if none of the new or modified cases are being called and returns to the original code
+    nop
+case20:
+    jal     kaleidoScope_Case20
+    or      a0, s1, zero
+    b       returnFromSwitchHack        ; jumps to return section of kaleidoScope_Update
+    ori     v0, zero, 0
+case21:
+    jal     kaleidoScope_Case21
+    or      a0, s1, zero
+    b       returnFromSwitchHack         ; jumps to return section of kaleidoScope_Update
+    ori     v0, zero, 0
+case22:
+    b       returnFromSwitchHack              ; jumps to case 17
+    ori     v0, zero, 2
+return_no_new_cases:
+    ori     v0, zero, 1
+returnFromSwitchHack:
+    ori     at, zero, 2
+    lw      ra, 0x0010(sp)
+    jr      ra
+    addiu   sp, sp, 0x0014
+
+KaleidoScope_DrawPages_ConditionHack:
+    addiu   sp, sp, -0x0014
+    sw      ra, 0x0010(sp)
+    addiu   at, zero, 7
+    beq     a0, at, KaleidoScope_DrawPages_ConditionHack_continue
+    addiu   at, zero, 21
+    beq     a0, at, KaleidoScope_DrawPages_ConditionHack_continue
+    addiu   at, zero, 20
+    beq     a0, at, KaleidoScope_DrawPages_ConditionHack_continue
+    slti    at, a0, 0x0008
+    bne     at, zero, KaleidoScope_DrawPages_ConditionHack_skip
+    slti    at, a0, 0x0012
+    beql    at, zero, KaleidoScope_DrawPages_ConditionHack_skip
+    nop
+KaleidoScope_DrawPages_ConditionHack_continue:
+    b       KaleidoScope_DrawPages_ConditionHack_return
+    ori     at, zero, 0
+KaleidoScope_DrawPages_ConditionHack_skip:
+    ori     at, zero, 1
+    b       KaleidoScope_DrawPages_ConditionHack_return
+KaleidoScope_DrawPages_ConditionHack_return:
+    lw      ra, 0x0010(sp)
+    jr      ra
+    addiu   sp, sp, 0x0014
+
+KaleidoScope_Draw_ContinuePromtHack:
+    addiu   sp, sp, -0x0014
+    sw      ra, 0x0010(sp)
+    addiu   at, zero, 0x10
+    or      s6, zero, a0
+    beq     a0, at, KaleidoScope_Draw_Texture_Logic
+    li      at, 0x14
+    beq     a0, at, KaleidoScope_Draw_Texture_Logic
+    li      at, 0x15
+    beq     a0, at, KaleidoScope_Draw_Texture_Logic
+    li      at, 0x11
+    bnel    a0, at, KaleidoScope_Draw_ContinuePromtHack_skip
+    lw      v1, 0x2C0(s0)
+KaleidoScope_Draw_Texture_Logic:
+    lui     t6, 0x8012
+    lbu     t6, -0x4627(t6)
+    lw      a0, 0x02C0(s0)
+    sll     t8, t6, 2
+    addu    a1, a1, t8
+    ori     at, zero, 0x13
+    slt     at, at, s6
+    ori     s6, zero, 0
+    beqz    at, KaleidoScope_TexUnchanged
+    nop
+    lui     a1, hi(ICON_STATIC_GAME_OVER)
+    addiu   a1, a1, lo(ICON_STATIC_GAME_OVER)
+    lw      a1, 0(a1)
+    addiu   a1, a1, 0x2980
+KaleidoScope_TexUnchanged:
+    addiu   a2, zero, 0x0098
+KaleidoScope_Draw_ContinuePromtHack_continue:
+    b       KaleidoScope_Draw_ContinuePromtHack_return
+    ori     at, zero, 0
+KaleidoScope_Draw_ContinuePromtHack_skip:
+    ori     at, zero, 1
+KaleidoScope_Draw_ContinuePromtHack_return:
+    lw      ra, 0x0010(sp)
+    jr      ra
+    addiu   sp, sp, 0x0014
+
+kaleidoScope_Set_yOffset:
+    addiu   sp, sp, -0x0014
+    sw      ra, 0x0010(sp)
+    slti    at, v0, 20
+    beqz    at, kaleidoScope_Set_yOffset_skip ; skip setting y
+    lui     at, 0x0001
+    ori     at, at, 0x0760
+    addu    t0, a0, at
+    addiu   t7, zero, 0x0050
+    sh      t7, 0x020A(t0)
+kaleidoScope_Set_yOffset_continue:
+    b       kaleidoScope_Set_yOffset_return
+    ori     at, zero, 0
+kaleidoScope_Set_yOffset_skip:
+    ori     at, zero, 1
+kaleidoScope_Set_yOffset_return:
+    lw      ra, 0x0010(sp)
+    jr      ra
+    addiu   sp, sp, 0x0014
+
+kaleidoScope_draw_select_hack:
+    addiu   sp, sp, -0x0014
+    sw      ra, 0x0010(sp)
+    ori     at, zero, 0x0007
+    beq     a1, at, kaleidoScope_draw_select_hack_continue
+    ori     at, zero, 0x0014
+    beq     a1, at, kaleidoScope_draw_select_hack_continue
+    addiu   at, at, 0x0001
+    beq     a1, at, kaleidoScope_draw_select_hack_continue
+    nop
+kaleidoScope_draw_select_hack_skip:
+    b       kaleidoScope_draw_select_hack_return
+    ori     at, zero, 1
+kaleidoScope_draw_select_hack_continue:
+    ori     at, zero, 0
+kaleidoScope_draw_select_hack_return:
+    lh      t7, 0x0238(t0)
+    lui     t2, 0x8012
+    lw      ra, 0x0010(sp)
+    jr      ra
+    addiu   sp, sp, 0x0014
+
 ICON_STATIC_GAME_OVER:
 .word 0x00000000
 
 CFG_SAVE_AND_QUIT:
 .byte 0x00
-
-.align 0x10
-CONTINUE_PROMPT_TEX:
-.area 0x980, 0
-.endarea
